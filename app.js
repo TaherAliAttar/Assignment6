04c75a803eb43ce296a79785082641eb620f0dd1
Taher Ali. Attar <taher@talentica-all.com>
2018-09-27 15:58:57 +0530

    Assignment6

---
+++ app.js
@@ -0,0 +1,89 @@
+// starting server with port 4000 to listen for incoming requests.
+var express=require('express'),
+app=express(),
+server=require('http').createServer(app),
+io=require('socket.io').listen(server);
+users = {};
+
+// set view engine as ejs.
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.get('/', function(req,res){
+    res.render('index');
+});
+var port = process.env.PORT || 3500;
+    server.listen(port);
+
+
+// defining different event for socket.io
+    io.sockets.on('connection', function(socket){
+    console.log("New Connection Established");  
+
+    socket.on('new user', function(data,callback){
+        if(data in users){
+            console.log("User already taken.");
+            callback(false);
+        }
+        else{
+            console.log("User available");
+            callback(true);
+            socket.nickname = data;
+            users[socket.nickname]= socket;
+            updateNicknames();
+        }
+    });  
+    function updateNicknames(){
+        io.sockets.emit('usernames',Object.keys(users));            
+       
+    }
+
+    socket.on('send message',function(data,callback){
+        var msg=data.trim();
+
+        if(msg.substr(0,1) === '@'){
+          msg=msg.substr(1);
+          var ind=msg.indexOf(' ');
+          if(ind !== -1){
+            var name=msg.substring(0,ind);
+            var msg=msg.substring(ind+1);
+             if(name in users){
+                users[name].emit('whisper',{msg:msg,nick:socket.nickname});
+                socket.emit('private',{msg:msg,nick:name});
+              console.log("Whispering !");
+            }else{
+              callback("Sorry, "+name+" is not online");
+            }
+          }
+          else{
+            callback("Looks like you forgot to write the message");
+          }
+        }
+         else
+         {
+         console.log("Got Message :"+ data);
+         io.sockets.emit('new message',{msg:msg,nick:socket.nickname});
+           }
+      });
+
+      socket.on('disconnect',function(data){
+        if(!socket.nickname) return;
+        delete users[socket.nickname];
+        updateNicknames();
+  });
+  socket.on('base64 file', function (msg) {
+    console.log('received base64 file from' + msg.username);
+    socket.username = msg.username;
+    // socket.broadcast.emit('base64 image', //exclude sender
+    io.sockets.emit('base64 file',  //include sender
+
+        {
+          username: socket.username,
+          file: msg.file,
+          fileName: msg.fileName
+        }
+
+    );
+});
+});      
+        
+    
